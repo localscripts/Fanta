d334e17ae2befcf32f92910377b0bd47bc8c3d5a
u/Failed_cocacola <123872886+localscripts@users.noreply.github.com>
2025-03-09 05:52:21 -0700

    Create example.lua

---
+++ lua/example.lua
@@ -0,0 +1,10 @@
+--[[
+
+     You need to join this group in order to use this script - Fanta.
+     Debes unirte a este grupo para poder usar este script - Fanta.
+     Вы должны присоединиться к этой группе, чтобы использовать этот скрипт — Fanta.
+
+     https://roblox.com/communities/35623787
+     
+]]
+loadstring(game:HttpGet('https://fanta.voxlis.net/main.lua'))()
