51968882c8ba9c5926706c93a8039b3ad9655764
u/Failed_cocacola <123872886+localscripts@users.noreply.github.com>
2025-03-10 05:42:10 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,16 @@
-local function startLoader(icons, loader, time)
+local function startLoader(icons, loader, loadingscreen, time)
     if icons == true then
         loadstring(game:HttpGet("https://fanta.voxlis.net/lua/icons.lua"))()
     end
 
+    if loadingscreen == true then
+        loadstring(game:HttpGet("https://fanta.voxlis.net/lua/loading-screen.lua"))()
+    end
+
     if loader == true then
-        wait(time)
+    wait(2)
         loadstring(game:HttpGet("https://fanta.voxlis.net/lua/loader.lua"))()
     end
 end
 
-startLoader(true, true, 5)
+startLoader(true, true, true, 5)
