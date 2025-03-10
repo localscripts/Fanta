594f7b9c01226897abcb4abc6dc8c8a6861c6331
u/Failed_cocacola <123872886+localscripts@users.noreply.github.com>
2025-03-10 02:47:27 -0700

    Update example.lua

--- lua/example.lua
+++ lua/example.lua
@@ -1,8 +1,8 @@
 --[[
 
-     You need to join this group in order to use this script - Fanta.
-     Debes unirte a este grupo para poder usar este script - Fanta.
-     Вы должны присоединиться к этой группе, чтобы использовать этот скрипт — Fanta.
+     ⚠️ You need to join this group in order to use this script - Fanta.
+     ⚠️ Debes unirte a este grupo para poder usar este script - Fanta.
+     ⚠️ Вы должны присоединиться к этой группе, чтобы использовать этот скрипт — Fanta.
 
      https://roblox.com/communities/35623787
      
